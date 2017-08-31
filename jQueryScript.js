a6fe59ce7b0703c05fd02069a9f02ff6370dc890
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-08-31 12:26:41 -0500

    completed project

--- jQueryScript.js
+++ jQueryScript.js
@@ -5,5 +5,52 @@
 //it will invoke the function once the document is ready and loaded, which is what the ready() function does
 
 $(document).ready(function(){
-  
+
+  //login example
+  var user = "Jacob"
+  var pass = "hey"
+
+  //use button and the .click to assign values and check for accuracy
+  $('button').click(function(){
+    var username = $('#input')
+    var password = $('#password')
+    
+    if(!username.val()){
+      username.css('border','2px solid red')
+      $('#container').prepend(
+        '<p id="error_msg" style="color:red; font-size:12px;"> Please ensure username is provided </p>'
+      );
+    }
+
+    if(!password.val()){
+      password.css('border','2px solid red')
+      $('#container').prepend(
+        '<p id="error_msg" style="color:red; font-size:12px;"> Please ensure password is provided </p>'
+      );
+    }
+
+    if(username.val() && password.val()){
+
+      username.css({
+        'border-width':'2px',
+        'border-style':'inset',
+        'border-color':'initial',
+        'border-image': 'initial'
+      })
+
+      password.css({
+        'border-width':'2px',
+        'border-style':'inset',
+        'border-color':'initial',
+        'border-image': 'initial'
+      })
+
+      if (username.val() === user && password.val() === pass) {
+        alert("You're in!");
+      } else $('#container').prepend(
+        '<p id="error_msg" style="color:red; font-size:12px;"> Please ensure your login info is correct </p>'
+      );
+    }
+  })
+
 })
